dca5e09a14c76bfcc12f766d52cf36a30ecdf8a0
Kai2862 <kaifengl2862@gmail.com>
2025-05-22 08:14:35 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,11 @@
+public int dogwalker(hour){
+    int dawgs = company.num.AvailableDogs(hour);
+    if(maxDog>=dawgs){
+        company.updateDogs(hour, dawgs);
+        return dawgs;
+    }
+    comapny.updateDogs(hour,maxDog);
+    return maxDog
 
+
+}
