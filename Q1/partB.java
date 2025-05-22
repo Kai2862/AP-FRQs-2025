1c13429c4d2865b20df37b7d5241adb5391bb63b
Kai2862 <kaifengl2862@gmail.com>
2025-05-22 08:25:54 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,11 @@
+public int dogWalkShift(int startHour, int endHour) {
+    int mula=0;
+    for (int hour=startHour; hour<=endHour;hour++) {
+        int dogs = walkDogs(hour);
+        mula += dogs*5;
 
+        if (dogs==maxDogs||(hour>=9&&hour<= 17)) {
+            mula += 3;
+        }
+    }
+    return mula;
