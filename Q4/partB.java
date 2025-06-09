55d873c98097aa666e69c2cd3a3fd7cc480fbdcd
Kai2862 <kaifengl2862@gmail.com>
2025-06-09 11:05:04 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -3,7 +3,7 @@ public boolean clearPair(int row, int col){
     for(int i =0;i<puzzle.length;i++){
         for(int j = 0;j<puzzle[0].length;j++){
             int current = puzzle[i][j]
-            if((i >= row)&&(row != i || col != j)&&(m == current||m + current == 10)){
+            if((i >= row)&&(row != i || col != j)&&(val == current||val + current == 10)){
                 puzzle[i][j]=0;
                 puzzle[row][col]=0;
                 return true;
