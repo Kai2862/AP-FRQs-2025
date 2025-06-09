7795eaa615f7d128fdc12684f765d86bc473f85e
Kai2862 <kaifengl2862@gmail.com>
2025-06-09 10:40:48 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,9 @@
+public SumOfSameGames(int NumRows, int NumCol){
+    puzzle = new int[numRows][numCols];
+    for(int i =0;i<numRows;i++){
+        for(int j = 0;j<numCol;j++){
+            puzzle[row][col]= (int)(Math.random()*9)+1;
+        }
+    }
 
+}
